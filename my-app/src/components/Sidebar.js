a850bb0460d45cc9ebfaa3870be917bcd358eb18
Lucimeri Andretta <luandretta@hotmail.com>
2023-06-14 07:59:32 +0000

    Feat: create Sidebar component

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sidebar = () => {
+     return (
+        <div>
+            <a href = "#." target= "_blank">My Photos</a>
+            <a href = "#." target= "_blank">My Illustrations</a>
+            <a href = "#." target= "_blank">My Paintings</a>
+        </div>
+    )
+}
+
+export default Sidebar;

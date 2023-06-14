1694d255c3b9bf296a1a571686c54a91432b52f7
Lucimeri Andretta <luandretta@hotmail.com>
2023-06-14 12:53:50 +0000

    Feat: create navbar component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
+import NavBarSimple from './components/NavBarSimple';
 
 function App() {
   return (
     <div className={css.App}>
       {}
+      <NavBarSimple />
       <Sidebar />
     </div>
   );

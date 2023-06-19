b0228c6a18aad384929e4fe9e10797f179a1db8b
Lucimeri Andretta <luandretta@hotmail.com>
2023-06-19 07:35:07 +0000

    Feat: create searchbar

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -9,27 +9,52 @@ export class Content extends Component {
         super(props);
         this.state = {
             isLoaded: false,
+            posts: [],
         }
+
     }
 
     componentDidMount() {
         setTimeout(()=>{
             this.setState({
                 isLoaded: true,
+                posts: savedPosts,
             })
         }, 2000)
+    }
+
+    handleChange = (e) => {
+        const name = e.target.value.toLowerCase();
+        const filteredPosts = savedPosts.filter((post)=>{
+            return post.name.toLowerCase().includes(name);
+        })
+        
+        this.setState({
+            posts: filteredPosts
+        })
     }
     
     render() {
         return (
             <div className={css.Content}>
                 <div className={css.TitleBar}>
-                    <h1>My Photos</h1>
+                     <h1>My Photos</h1>
+                     <form>
+                        <label htmlForm="searchinput">Search:</label>
+                        <input 
+                        type="search" 
+                        id="searchinput"
+                        placeholder='By Author'
+                        onChange={(e) => this.handleChange(e)}
+                        ></input>
+                        <h4>posts found: {this.state.posts.length}</h4>
+                    </form>
+                   
                 </div>
                 <div className={css.SearchResults}>
                     {
                         this.state.isLoaded ?
-                        <PostItem savedPosts={savedPosts} />
+                        <PostItem savedPosts={this.state.posts} />
                         : <Loader />
                     }
     
